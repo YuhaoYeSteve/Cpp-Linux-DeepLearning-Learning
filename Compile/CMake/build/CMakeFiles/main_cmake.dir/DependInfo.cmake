4e9e2f45723087d5be1421253d09ec47fa35833e
yyh <yeyuhaosteve@163.com>
2023-11-14 16:12:37 +0800

    hi

--- Compile/CMake/build/CMakeFiles/main_cmake.dir/DependInfo.cmake
+++ Compile/CMake/build/CMakeFiles/main_cmake.dir/DependInfo.cmake
@@ -4,14 +4,14 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/ps/C++/Cpp-Linux-DeepLearning-Learning/Compile/CMake/main.cpp" "/home/ps/C++/Cpp-Linux-DeepLearning-Learning/Compile/CMake/CMakeFiles/main_cmake.dir/main.cpp.o"
-  "/home/ps/C++/Cpp-Linux-DeepLearning-Learning/Compile/CMake/src/sum.cpp" "/home/ps/C++/Cpp-Linux-DeepLearning-Learning/Compile/CMake/CMakeFiles/main_cmake.dir/src/sum.cpp.o"
+  "/home/ps/C++/Cpp-Linux-DeepLearning-Learning/Compile/CMake/main.cpp" "/home/ps/C++/Cpp-Linux-DeepLearning-Learning/Compile/CMake/build/CMakeFiles/main_cmake.dir/main.cpp.o"
+  "/home/ps/C++/Cpp-Linux-DeepLearning-Learning/Compile/CMake/src/sum.cpp" "/home/ps/C++/Cpp-Linux-DeepLearning-Learning/Compile/CMake/build/CMakeFiles/main_cmake.dir/src/sum.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
-  "include"
+  "../include"
   )
 
 # Targets to which this target links.
--- Compile/CMake/build/cmake_install.cmake
+++ Compile/CMake/build/cmake_install.cmake
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/ps/C++/Cpp-Linux-DeepLearning-Learning/Compile/CMake/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/ps/C++/Cpp-Linux-DeepLearning-Learning/Compile/CMake/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
