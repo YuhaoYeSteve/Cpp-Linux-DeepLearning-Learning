cc4ba8da16c14951e48bf5493686b17e5075b6b6
yyh <yeyuhaosteve@163.com>
2023-11-17 01:05:13 +0800

    mac

--- Compile/CMake/build/CMakeFiles/Makefile.cmake
+++ Compile/CMake/build/CMakeFiles/Makefile.cmake
@@ -42,5 +42,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/main_cmake.dir/DependInfo.cmake"
+  "CMakeFiles/main_cmake_gdb.dir/DependInfo.cmake"
   )
--- Compile/CMake/main.cpp
+++ Compile/CMake/main.cpp
@@ -6,6 +6,8 @@ int main(){
     cout<<"c:"<<c<<endl;
     int a = 2;
     int b = 3;
+    // int c= 4;
+    cout << "testnihao " << endl;
     cout <<"sum(a,b):" <<sum(a,b) <<endl;
     return 0;
 }
